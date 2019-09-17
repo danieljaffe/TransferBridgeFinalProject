b095b4bd87df93095cec933fb078c4b7d0c83958
Brenden <brenden728@ucla.edu>
2019-09-17 11:06:24 -0700

    Implemented scroll and draw functions

--- FinalProject/FinalProject/Map.cpp
+++ FinalProject/FinalProject/Map.cpp
@@ -1,26 +1,65 @@
 #include "Map.h"
 #include <iostream>
+#include <time.h>
+#include <cstdlib>
+#include <math.h>
 using namespace std; 
 
 //Constructor
 Map::Map(){
+	srand(time(0));
 	//init top and bottom to # sign
-	for (int i = 0; i < n_columns; i++) {
-		m_map[0][i] = '#';
-		m_map[n_rows][i] = '#';
+	for (int i = 0; i < n_rows; i++) {
+		for (int j = 0; j < n_columns; j++) {
+			m_map[i][j] = Map::SPACE; 
+		}
 	}
+
+	for (int i = 0; i < n_columns; i++) {
+		m_map[0][i] = Map::WALL;
+		m_map[n_rows-1][i] = Map::WALL;
+	}
+	// set null termination 
+	for (int i = 0; i < n_rows; i++) {
+		m_map[i][Map::n_columns] = '\0';
+		//m_map[i][n_columns - 1] = Map::WALL;
+	}
+	
 }
 
-Map::~Map() {
-
-}
 
 void Map::scroll() {
+	int min = 1 + Map::MIN_WALL_DIST / 2;
+	int max = n_rows - 2 - Map::MIN_WALL_DIST / 2;
+	int offset = min + (rand() % (max - min));
 
+	for (int i = 0; i < 4; i++)
+	{
+		offset += tunnelAmps[i] * sin(tunnelFreqs[i] * scrollIndex);
+	}
+
+	//shift everything over left 1 
+	for (int i = 0; i < n_rows; i++) {
+		for (int j = 0; j < n_columns - 1; j++) {
+			m_map[i][j] = m_map[i][j + 1];
+		}
+	}
+	//set new column 
+	for (int i = 0; i < n_rows; i++) {
+		if (abs(i - offset) > MIN_WALL_DIST / 2) {
+			m_map[i][n_columns - 1] = Map::WALL;
+		}
+		else {
+			m_map[i][n_columns - 1] = Map::SPACE; 
+		}
+	}
+
+	++scrollIndex;
 }
 
 void Map::draw() {
 	for (int i = 0; i < n_rows; i++) {
 		cout << m_map[i] << endl; 
 	}
+	
 }
--- FinalProject/FinalProject/Map.h
+++ FinalProject/FinalProject/Map.h
@@ -1,18 +1,25 @@
-//test
 #ifndef MAP
 class Map
 {
 public:
 	Map();
-	~Map();
+	~Map()=default;
 	void draw();
 	void scroll();
-
-
+	static const int n_rows = 28;
+	static const int n_columns = 100;
+	static const int MIN_WALL_DIST = int(n_rows / 1.6);
 
 private:
-	static const int n_rows = 75; 
-	static const int n_columns = 22; 
-	char m_map[n_rows + 1][n_columns + 1] = {0};
+
+	static const char WALL = '#';
+	static const char SPACE = ' ';
+
+	unsigned int scrollIndex = 0;
+
+	double tunnelFreqs[3] = {0.025, 0.05, 0.10 };
+	double tunnelAmps[3] = {5.0, 2.0, 2.0 };
+	
+	char m_map[n_rows][n_columns + 1] = {0};
 };
 #endif 
