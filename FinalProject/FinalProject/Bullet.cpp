cd0bc281fb9802b51a63e161d33f4e84c12b90d0
Daniel Jaffe <flarros@gmail.com>
2019-09-17 22:50:58 -0700

    Temp Bullet

--- FinalProject/FinalProject/Bullet.cpp
+++ FinalProject/FinalProject/Bullet.cpp
@@ -1,6 +1,6 @@
 #include "Bullet.h"
 
-Bullet::Bullet()
+Bullet::Bullet(Game* game, int hp, int arm, int pwr, int rate, char character, int x, int y) : Actor(game, hp, arm, pwr, rate, character, x, y)
 {
 
 }
@@ -12,6 +12,33 @@ Bullet::~Bullet()
 void Bullet::attack(Actor*)
 {
 }
+
+
+//void Bullet::move(bool playerShot)
+//{
+//
+//	int x = getPosition()->getX();
+//	int y = getPosition()->getY();
+//	switch (input)
+//	{
+//	case 'w':
+//	case 'W': y--;
+//		break;
+//	case 'a':
+//	case 'A': x--;
+//		break;
+//	case 's':
+//	case 'S': y++;
+//		break;
+//	case 'd':
+//	case 'D': x++;
+//		break;
+//	default:
+//		break;
+//	}
+//	getPosition()->setX(x);
+//	getPosition()->setY(y);
+//}
 
 void Bullet::move()
 {
