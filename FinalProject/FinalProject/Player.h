5dcafdee80be5e1ef0e23ea0be29edcd5548a57b
Daniel Jaffe <flarros@ucla.edu>
2019-09-18 15:36:59 -0700

    Cool

--- FinalProject/FinalProject/Player.h
+++ FinalProject/FinalProject/Player.h
@@ -8,7 +8,7 @@ class Game;
 class Player : public Actor {
 public:
 	// Constructors and Destructors
-	Player(Game*, int hp=100, int arm=50, int pwr=1, int rate=1, char character='@', int x=5, int y=17);
+	Player(Game*, int hp=20, int arm=5, int pwr=1, int rate=1, char character='@', int x=5, int y=17);
 	virtual ~Player();
 
 	// Member functions
